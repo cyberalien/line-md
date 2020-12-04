b732fde1210a4829755277060b43bf12404f4c47
Vjacheslav Trushkin <cyberalien@gmail.com>
2020-12-04 11:48:32 +0200

    Add function rmdir to build process helpers

--- build/fs.js
+++ build/fs.js
@@ -11,15 +11,59 @@ function mkdir(dir) {
 		return;
 	}
 	let current = rootDir;
-	const extra = dir.slice(rootDir.length).split('/');
-	while (extra.length) {
+	const parts = dir.slice(rootDir.length).split('/');
+	while (parts.length) {
 		try {
 			fs.mkdirSync(dir, 0o755);
 		} catch (err) {}
-		const next = extra.shift();
+		const next = parts.shift();
 		if (next !== '') {
 			current += '/' + next;
 		}
 	}
 }
 exports.mkdir = mkdir;
+
+/**
+ * Remove directory recursively
+ */
+function rmdir(dir) {
+	function removeFiles(dir) {
+		let files;
+
+		try {
+			files = fs.readdirSync(dir);
+		} catch (err) {
+			return;
+		}
+
+		files.forEach((file) => {
+			let filename = dir + '/' + file,
+				stats = fs.lstatSync(filename);
+
+			if (stats.isDirectory()) {
+				removeFiles(filename);
+
+				// Try to remove directory
+				try {
+					fs.rmdirSync(filename);
+				} catch (err) {}
+				return;
+			}
+
+			if (stats.isFile() || stats.isSymbolicLink()) {
+				// Try to remove file
+				try {
+					fs.unlinkSync(filename);
+				} catch (err) {}
+				return;
+			}
+		});
+	}
+
+	removeFiles(dir);
+	try {
+		fs.rmdirSync(dir);
+	} catch (err) {}
+}
+exports.rmdir = rmdir;
