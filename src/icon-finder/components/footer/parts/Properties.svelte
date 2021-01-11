ca9954f70b3effc1309457cb4b2035b52caafb94
Vjacheslav Trushkin <cyberalien@gmail.com>
2021-01-11 15:26:58 +0200

    Restructure: remove svelte -> js -> svelte imports. Using named imports for components fails with Sapper

--- src/icon-finder/components/footer/parts/Properties.svelte
+++ src/icon-finder/components/footer/parts/Properties.svelte
@@ -12,13 +12,11 @@
 		customiseFlip,
 		customiseInline,
 	} from '../../../config/components';
-	import {
-		ColorBlock,
-		SizeBlock,
-		RotateBlock,
-		FlipBlock,
-		InlineBlock,
-	} from '../../../config/footer-components';
+	import ColorBlock from './props/color/Color.svelte';
+	import SizeBlock from './props/size/Size.svelte';
+	import RotateBlock from './props/rotate/Rotate.svelte';
+	import FlipBlock from './props/flip/Flip.svelte';
+	import InlineBlock from './props/inline/Inline.svelte';
 
 	// Selected icons
 	export let icons: Icon[];
