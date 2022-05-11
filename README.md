8bf300511d9f1a564a2a3f460af9c0769ef84e70
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-05-11 10:56:08 +0300

    Add dev files

---
+++ README.md
@@ -0,0 +1,57 @@
+# Material Line Icons
+
+There are many icon sets that are designed to Material Design guidelines. You can find icons for anything you need.
+
+This icon set is different:
+
+- Icons are animated using CSS animations for stroke.
+- It follows guidelines that are used for 'Round' and 'TwoTone' icons in the official Material Design Icons set.
+- Sometimes 1px thin lines are used where it makes sense.
+
+Preview is available at https://cyberalien.github.io/line-md/
+
+Icon set is in development. It contains only few icons so far. Icons can change any time.
+
+# CSS stroke animations
+
+Icons use stroke that can be animated on first render. All you have to do is add class "iconify--line-md" to SVG element (Iconify SVG framework 2.0 does it automatically) and include stylesheet from `https://code.iconify.design/css/line-md.css`.
+
+To refresh animation you can either re-render icon or use display to hide and show it.
+
+# Usage in HTML
+
+1. Add stylesheet for CSS animations:
+
+```html
+<link rel="stylesheet" href="https://code.iconify.design/css/line-md.css" />
+```
+
+2. Include Iconify SVG framework:
+
+```html
+<script src="https://code.iconify.design/2/2.0.0-rc.1/iconify.min.js"></script>
+```
+
+3. Add placeholder for icon, using "line-md" prefix:
+
+```html
+<span class="iconify" data-icon="line-md:home"></span>
+```
+
+See [Iconify SVG framework documentation](https://docs.iconify.design/implementations/svg-framework/) for details.
+
+# Components
+
+React, Vue and Svelte components are available. See [Iconify components documentation](https://docs.iconify.design/implementations/components/) for details.
+
+# Icon requests
+
+You can [open an issue on GitHub repository](https://github.com/cyberalien/line-md/issues) to request icons.
+
+Icons are currently designed in Figma and exported using custom Figma plug-in. Plug-in calculates lengths of all shapes, order of animations and assigns appropriate classes to shapes within exported SVG.
+
+At moment of writing this, Figma has a bug with exporting shapes that require masking (it replaces stroke with fill and merges shape with mask), which means complex shapes cannot be animated, so icons that require masks cannot be added yet.
+
+# Licence
+
+MIT
---
+++ build.js
@@ -0,0 +1,480 @@
+const { promises: fs } = require('fs');
+const {
+	IconSet,
+	importDirectory,
+	runSVGO,
+	deOptimisePaths,
+} = require('@iconify/tools');
+
+// Legacy icons that are no longer in set
+const legacyIcons = new Set(['iconify2']);
+
+// Categories for all icons
+const categories = {
+	Arrows: [
+		'arrow-left',
+		'arrow-small-left',
+		'arrow-left-circle',
+		'arrow-left-circle-twotone',
+		'chevron-left',
+		'chevron-left-circle',
+		'chevron-left-circle-twotone',
+		'chevron-double-left',
+		'chevron-triple-left',
+		'chevron-small-left',
+		'chevron-small-double-left',
+		'chevron-small-triple-left',
+		'arrow-open-left',
+		'arrow-close-left',
+		'arrows-horizontal',
+		'arrows-horizontal-alt',
+		'double-arrow-horizontal',
+		'navigation-left-up',
+		'navigation-left-down',
+		'navigation-right-up',
+		'navigation-right-down',
+		'arrows-diagonal',
+		'arrow-long-diagonal',
+	],
+	Alerts: [
+		'alert',
+		'alert-twotone',
+		'alert-circle',
+		'alert-circle-twotone',
+		'question',
+		'question-circle',
+		'question-circle-twotone',
+		'bell',
+		'bell-twotone',
+	],
+	Editing: [
+		'grid-3',
+		'grid-3-twotone',
+		'grid-3-solid',
+		'list-3',
+		'list-3-twotone',
+		'list-3-solid',
+		'list',
+		'list-indented',
+		'check-list-3',
+		'check-list-3-twotone',
+		'check-list-3-solid',
+		'paint-drop',
+		'paint-drop-half-filled',
+		'paint-drop-half-twotone',
+		'paint-drop-half-filled-twotone',
+		'paint-drop-filled',
+		'paint-drop-twotone',
+		'rotate-270',
+		'rotate-180',
+		'rotate-90',
+		'align-center',
+		'align-justify',
+		'align-left',
+		'valign-baseline',
+		'valign-baseline-twotone',
+		'valign-middle',
+		'valign-middle-twotone',
+		'valign-top',
+		'valign-top-twotone',
+		'valign-bottom',
+		'valign-bottom-twotone',
+		'arrow-align-left',
+		'arrow-align-center',
+		'edit',
+		'edit-twotone',
+		'edit-twotone-full',
+		'pencil',
+		'pencil-twotone',
+		'pencil-twotone-alt',
+		'marker',
+		'marker-twotone',
+		'marker-filled',
+		'clipboard',
+		'clipboard-twotone',
+		'clipboard-check',
+		'clipboard-check-twotone',
+		'clipboard-plus',
+		'clipboard-plus-twotone',
+		'clipboard-minus',
+		'clipboard-minus-twotone',
+		'clipboard-list',
+		'clipboard-list-twotone',
+		'clipboard-arrow',
+		'clipboard-arrow-twotone',
+		'document',
+		'document-twotone',
+		'document-add',
+		'document-add-twotone',
+		'document-remove',
+		'document-remove-twotone',
+		'document-list',
+		'document-list-twotone',
+		'document-code',
+		'document-code-twotone',
+		'document-report',
+		'document-report-twotone',
+		'image',
+		'image-twotone',
+	],
+	Interface: [
+		'search',
+		'search-twotone',
+		'search-filled',
+		'remove',
+		'close',
+		'close-circle',
+		'close-circle-twotone',
+		'confirm',
+		'confirm-circle',
+		'confirm-circle-twotone',
+		'plus',
+		'plus-circle',
+		'plus-circle-twotone',
+		'minus',
+		'minus-circle',
+		'minus-circle-twotone',
+		'circle',
+		'circle-twotone',
+		'cancel',
+		'cancel-twotone',
+		'construction',
+		'construction-twotone',
+	],
+	Navigation: [
+		'home-md',
+		'home-md-twotone',
+		'home-md-twotone-alt',
+		'home',
+		'home-twotone',
+		'home-twotone-alt',
+		'home-simple',
+		'home-simple-twotone',
+		'home-simple-filled',
+		'external-link',
+		'external-link-rounded',
+		'hash',
+		'hash-small',
+		'menu-unfold-left',
+		'menu-unfold-right',
+		'menu-fold-left',
+		'menu-fold-right',
+		'menu',
+		'calendar',
+		'calendar-out',
+	],
+	Emoji: [
+		'emoji-smile',
+		'emoji-smile-twotone',
+		'emoji-grin',
+		'emoji-grin-twotone',
+		'emoji-smile-wink',
+		'emoji-smile-wink-twotone',
+		'emoji-neutral',
+		'emoji-neutral-twotone',
+		'emoji-frown',
+		'emoji-frown-twotone',
+		'emoji-frown-open',
+		'emoji-frown-open-twotone',
+		'emoji-angry',
+		'emoji-angry-twotone',
+	],
+	Social: [
+		'linkedin',
+		'twitter',
+		'twitter-twotone',
+		'facebook',
+		'github',
+		'github-twotone',
+		'github-loop',
+		'heart',
+		'heart-twotone',
+		'heart-twotone-half',
+		'heart-filled',
+		'heart-filled-half',
+		'heart-twotone-half-filled',
+		'heart-half',
+		'heart-half-twotone',
+		'heart-half-filled',
+		'thumbs-up',
+		'thumbs-up-twotone',
+		'thumbs-down',
+		'thumbs-down-twotone',
+		'iconify1',
+		'email',
+		'email-twotone',
+		'email-twotone-alt',
+		'email-opened',
+		'email-opened-twotone',
+		'email-opened-twotone-alt',
+	],
+	Technology: [
+		'cloud',
+		'cloud-twotone',
+		'cloud-filled',
+		'cloud-down',
+		'cloud-down-twotone',
+		'cloud-up',
+		'cloud-up-twotone',
+		'computer',
+		'computer-twotone',
+		'laptop',
+		'laptop-twotone',
+	],
+	Account: [
+		'account',
+		'account-small',
+		'account-add',
+		'account-remove',
+		'account-delete',
+		'account-alert',
+	],
+};
+
+// Sort categories
+const sortedCategories = Object.create(null);
+for (const title in categories) {
+	categories[title].forEach((name) => {
+		if (sortedCategories[name]) {
+			console.error(
+				`Multiple categories for ${name}: ${title} and ${sortedCategories[name]}`
+			);
+		}
+		sortedCategories[name] = title;
+	});
+}
+
+// Config for generating aliases
+const aliasesConfig = [
+	{
+		icons: [
+			'arrow-left',
+			'arrow-small-left',
+			'arrow-left-circle',
+			'arrow-left-circle-twotone',
+			'chevron-left',
+			'chevron-left-circle',
+			'chevron-left-circle-twotone',
+			'chevron-double-left',
+			'chevron-triple-left',
+			'chevron-small-left',
+			'chevron-small-double-left',
+			'chevron-small-triple-left',
+			'arrow-open-left',
+			'arrow-close-left',
+		],
+		search: '-left',
+		aliases: [
+			{
+				replace: '-right',
+				changes: {
+					hFlip: true,
+				},
+			},
+			{
+				replace: '-down',
+				changes: {
+					rotate: 3,
+				},
+			},
+			{
+				replace: '-up',
+				changes: {
+					rotate: 3,
+					hFlip: true,
+				},
+			},
+		],
+	},
+	{
+		icons: ['align-left', 'navigation-left-up'],
+		search: '-left',
+		replace: '-right',
+		changes: {
+			hFlip: true,
+		},
+	},
+	{
+		icons: ['arrow-align-left'],
+		search: '-left',
+		aliases: [
+			{
+				replace: '-right',
+				changes: {
+					hFlip: true,
+				},
+			},
+			{
+				replace: '-bottom',
+				changes: {
+					rotate: 3,
+				},
+			},
+			{
+				replace: '-top',
+				changes: {
+					rotate: 3,
+					hFlip: true,
+				},
+			},
+		],
+	},
+	{
+		icons: ['arrow-align-center'],
+		search: '-center',
+		replace: '-middle',
+		changes: {
+			rotate: 3,
+		},
+	},
+	{
+		icons: ['arrows-diagonal', 'arrow-long-diagonal'],
+		append: '-rotated',
+		changes: {
+			hFlip: true,
+		},
+	},
+	{
+		icons: [
+			'arrows-horizontal',
+			'arrows-horizontal-alt',
+			'double-arrow-horizontal',
+		],
+		search: '-horizontal',
+		replace: '-vertical',
+		changes: {
+			rotate: 1,
+		},
+	},
+	{
+		icons: ['navigation-left-up'],
+		search: '-left-up',
+		aliases: [
+			{
+				replace: '-left-down',
+				changes: {
+					vFlip: true,
+				},
+			},
+			{
+				replace: '-right-up',
+				changes: {
+					hFlip: true,
+				},
+			},
+			{
+				replace: '-right-down',
+				changes: {
+					rotate: 2,
+				},
+			},
+		],
+	},
+];
+
+(async () => {
+	// Load SVGs
+	const iconSet = await importDirectory('svg', {
+		prefix: 'line-md',
+		includeSubDirs: false,
+	});
+	iconSet.info = {
+		name: 'Material Line Icons',
+		author: {
+			name: 'Vjacheslav Trushkin',
+			url: 'https://github.com/cyberalien/line-md',
+		},
+		license: {
+			title: 'MIT',
+			spdx: 'MIT',
+			url: 'https://github.com/cyberalien/line-md/blob/master/license.txt',
+		},
+		height: 24,
+		samples: ['home', 'edit-twotone', 'image-twotone'],
+	};
+
+	// Make sure all icons are present
+	for (const name in sortedCategories) {
+		const category = sortedCategories[name];
+		if (!iconSet.entries[name] && !legacyIcons.has(name)) {
+			throw new Error(
+				`Missing icon "${name}", supposed to be in category "${category}"`
+			);
+		}
+		iconSet.toggleCategory(name, category, true);
+	}
+
+	// Validate categories
+	iconSet.forEach((name, type) => {
+		if (type !== 'icon') {
+			return;
+		}
+
+		// Find category
+		if (!sortedCategories[name]) {
+			console.error(`Missing category for "${name}"`);
+		}
+	});
+
+	// Add aliases
+	aliasesConfig.forEach((item) => {
+		const search = item.search;
+
+		item.icons.forEach((name) => {
+			const entry = iconSet.entries[name];
+			if (!entry || entry.type !== 'icon') {
+				throw new Error(`Cannot transform missing icon: "${name}"`);
+			}
+			if (search && name.indexOf(search) === -1) {
+				throw new Error(
+					`Cannot rename icon "${name}": expected to find "${search}"`
+				);
+			}
+
+			(item.aliases || [item]).forEach(({ replace, append, changes }) => {
+				if (!search && !append) {
+					throw new Error('Missing search or append');
+				}
+				const newName = search ? name.replace(search, replace) : name + append;
+
+				// Make sure icon does not exist
+				const entry = iconSet.entries[newName];
+				if (entry) {
+					if (entry.type !== 'icon') {
+						throw new Error(
+							`Cannot rename icon "${name}" to "${newName}": variation already exists`
+						);
+					}
+					return;
+				}
+
+				// Add icon and category
+				iconSet.setVariation(newName, name, changes);
+				const category = sortedCategories[name];
+				if (category) {
+					iconSet.toggleCategory(newName, category, true);
+				}
+			});
+		});
+	});
+
+	// Clean up icons
+	await iconSet.forEach(async (name, type) => {
+		if (type !== 'icon') {
+			return;
+		}
+
+		const svg = iconSet.toSVG(name);
+		// await runSVGO(svg, {
+		// 	keepShapes: true,
+		// });
+		// await deOptimisePaths(svg);
+		iconSet.fromSVG(name, svg);
+	});
+
+	// Export
+	const exported = iconSet.export();
+	const info = exported.info;
+	console.log(`Exported ${info.total} icons`);
+	await fs.writeFile('line-md.json', JSON.stringify(exported, null, '\t'));
+})();
